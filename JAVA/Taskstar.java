174f7ef8a8801b466fc9dae5b3da1f582b4f4c49
SARANYA <118508984+saranyab07@users.noreply.github.com>
2024-03-18 18:16:25 -1200

    Add files via upload

---
+++ JAVA/Taskstar.java
@@ -0,0 +1,15 @@
+public class Taskstar
+{
+ public static void main(String[]Args)
+ {
+  int star = 4;
+  for(int i=1;i<=star;i++)
+  {
+   for(int j=1;j<=i;j++)
+   {
+    System.out.print("* ");
+   }
+   System.out.println(" ");
+  }
+ }
+}
