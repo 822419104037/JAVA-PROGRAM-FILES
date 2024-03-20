8203e928752a39efb4daadae9a58dc41e537f80e
SARANYA <118508984+saranyab07@users.noreply.github.com>
2024-03-19 18:26:48 -1200

    Update ArrayListTask.java

--- JAVA/ArrayListTask.java
+++ JAVA/ArrayListTask.java
@@ -20,7 +20,31 @@ public class ArrayListTask
   task.remove(5);
   System.out.println(task);
 
-  //Finding max and min
-  int min = 
+ //Finding max and min
+  int min = task.get(0);
+  int max = task.get(0);
+
+  //length of the arraylist
+  int n = task.size();
+
+  //loop for minimum from arraylist
+  for(int i = 1;i < n; i++)
+  {
+   if(task.get(i) < min )
+   {
+    min = task.get(i);
+   }
+  }
+ 
+  //loop for maximum from arraylist
+  for(int i = 1;i < n; i++)
+  {
+   if(task.get(i) > max )
+   {
+    min = task.get(i);
+   }
+  }
+  System.out.println("Maximum is : "+max);
+  System.out.println("Minimum is : "+min); 
  }
 }
